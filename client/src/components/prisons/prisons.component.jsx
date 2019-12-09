0bfaa51169f2bec799027ec642e3f2d687e70f06
Aman <amsingh714@gmail.com>
2019-12-09 16:41:16 -0500

    able to render prison name

--- client/src/components/prisons/prisons.component.jsx
+++ client/src/components/prisons/prisons.component.jsx
@@ -1,32 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import "./prisons.styles.scss"; 
-import {fetchPrisons} from "../../redux/actions/prisons.actions"
+import "./prisons.styles.scss";
+import { fetchPrisons } from "../../redux/actions/prisons.actions";
 
 class Prisons extends Component {
-
   componentDidMount() {
     this.props.fetchPrisons();
   }
-  
-  
+
   render() {
     return (
       <div>
-        HI 
+        {this.props.prisons.map(prison => {
+          return <span>{prison.name}</span>;
+        })}
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
     prisons: state.prisons.prisons
-  }
-}
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  { fetchPrisons }
-)(Prisons); 
+export default connect(mapStateToProps, { fetchPrisons })(Prisons);
