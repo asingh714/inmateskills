a60e734274e61997417755c46f5f7fe7d4235e67
Aman <amsingh714@gmail.com>
2020-01-02 12:09:23 -0500

    added prison box styles

--- client/src/components/prisonBox/prisonBox.component.jsx
+++ client/src/components/prisonBox/prisonBox.component.jsx
@@ -13,11 +13,11 @@ const PrisonBox = ({ prison, ...props }) => {
   return (
     <div className="prison-box" onClick={e => routeToSinglePrisonProfilePage(e, prison.id)}>
       <img src={prison.prison_image} alt="Prison" className="prison-box-banner"/>
-      <span>{prison.name}</span>
-      <span> Inmates Available for Hire</span>
-      <span>{prison.address}</span>
-      <span>{prison.city}</span>
-      <span>{prison.state}</span>
+      <span className="prison-box-title">{prison.name}</span>
+      <span className="prison-box-count"> Inmates Available for Hire</span>
+      <span className="prison-box-street">{prison.address}</span>
+      <span className="prison-box-city-state">{prison.city}, {prison.state}</span>
+      <div className="prison-box-button">See More <span id="triangle">&#9658;</span></div>
     </div>
   );
 };
