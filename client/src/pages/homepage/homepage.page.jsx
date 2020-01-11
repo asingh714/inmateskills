f1a80f2ce18ee242d0ed0d480ad56b9c1f555639
Aman <amsingh714@gmail.com>
2020-01-11 14:30:30 -0700

    added hero image with text

--- client/src/pages/homepage/homepage.page.jsx
+++ client/src/pages/homepage/homepage.page.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import "./homepage.styles.scss"
+import "./homepage.styles.scss";
 
 export default class Homepage extends Component {
   render() {
     return (
       <div>
-        Homepage
+        <main className="hero-container">
+          <h1 className="main-header">We help find jobs for soon to be released inmates</h1>
+          <h2 className="sub-header">
+            A platform for prisons to broadcast skills of inmates, to help them
+            become more employable after prison
+          </h2>
+        </main>
       </div>
-    )
+    );
   }
 }
