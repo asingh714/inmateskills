f135cb5b466921859c36270f1b5796f4cbc1f13c
Aman <amsingh714@gmail.com>
2021-04-10 11:44:45 -0700

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -39,9 +39,9 @@ module.exports = {
     migrations: {
       directory: "./data/migrations"
     },
-    seeds: {
-      directory: "./data/seeds"
-    },
+    // seeds: {
+    //   directory: "./data/seeds"
+    // },
     dialect: "postgres",
     dialectOptions: {
       ssl: true,
